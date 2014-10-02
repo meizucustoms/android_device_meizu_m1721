33b13a8348213455c812a4b1a5216bedcd5bf283
deadman96385 <seanhoyt963@gmail.com>
2014-10-02 04:12:12 +0000

    config: Add 3GB dalvik heap and hwui memory
    Should be used on devices with 3gb's of ram (Bacon, LG G3, etc)
    Change-Id: I5833a19ea77b99009fc9f9c7c2a967b0dde06b77
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- configs/memconfig/phone-xxhdpi-3072-dalvik-heap.mk
+++ configs/memconfig/phone-xxhdpi-3072-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a 3G phone
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=24m \
-    dalvik.vm.heapgrowthlimit=320m \
-    dalvik.vm.heapsize=768m \
+    dalvik.vm.heapstartsize=8 \
+    dalvik.vm.heapgrowthlimit=384m \
+    dalvik.vm.heapsize=1024m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
