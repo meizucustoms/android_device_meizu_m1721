52372ff38c8f9baeffe5827d3fbc5d752c03e83c
Alex Naidis <alex.naidis@linux.com>
2016-08-14 21:48:27 -0400

    dalvik: rework 3 and 4-GB dalvik heap configurations
    The previous configuration sets target utilization as .25, which is geared towards
    low memory devices. This path increases it to .75 and makes us pass the check:
    (heaptargetutilization / 2) * heapsize = heapgrowthlimit
    
    Example:
    heapgrowthlimit: 256m
    heapsize: 512m
    heaptargetutilization: 0.75
    0.75/2 * 512 = 192
    
    To pass the check this has to be true:
    
    192 = 256 (WRONG)
    
    Check not passed.
    
    This new configuration is optimized for higher RAM devices and passes the check:
    
    heapgrowthlimit: 384m
    heapsize: 1024m
    heaptargetutilization: 0.75
    
    0.75/2 * 1024 = 384
    
    384 = 384 (TRUE)
    
    Check passed.
    
    PS2: Values corrected. Thanks Alex.
    https://github.com/CyanogenMod/android_frameworks_native/commit/30bc76d65791e92b1385c44f0a561cea1b4e68cb
    https://github.com/CyanogenMod/android_frameworks_native/commit/9ca91d6544faebd5f7f30e30f09b9aafe5b3e3b3
    
    Change-Id: I6839339382229da80546761c3746a032081ff2cd
    Signed-off-by: Alex Naidis <alex.naidis@linux.com>

--- configs/memconfig/phone-xxhdpi-3072-dalvik-heap.mk
+++ configs/memconfig/phone-xxhdpi-3072-dalvik-heap.mk
@@ -17,9 +17,9 @@
 # Provides overrides to configure the Dalvik heap for a 3G phone
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapstartsize=8 \
-    dalvik.vm.heapgrowthlimit=384m \
-    dalvik.vm.heapsize=1024m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=288m \
+    dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
     dalvik.vm.heapminfree=2m \
     dalvik.vm.heapmaxfree=8m
