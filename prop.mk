5055e108ee89226ce2c0618cb8dbe80a2f1fbf19
Shalaj Jain <shalajj@codeaurora.org>
2019-07-25 12:54:15 +0200

    mido: Enable Smooth Streaming for secure video playback
    Re-purpose previously unused property and enable smooth
    streaming for secure video playback.
    
    Signed-off-by: David Trpchevski <trpcevski.david@gmail.com>

--- prop.mk
+++ prop.mk
@@ -126,6 +126,7 @@ debug.stagefright.omx_default_rank.sw-audio=1 \
 debug.stagefright.omx_default_rank=0 \
 media.msm8956hw=0 \
 media.stagefright.audio.sink=280 \
+mm.enable.sec.smoothstreaming=true \
 media.stagefright.thumbnail.prefer_hw_codecs=true \
 mmp.enable.3g2=true \
 vendor.audio.hw.aac.encoder=true \
