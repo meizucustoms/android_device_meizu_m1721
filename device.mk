a6b39680f4149a9272859e7873c027de6794f1c8
Jyotiraditya <jyotiraditya@aospa.co>
2020-04-05 19:27:50 +0000

    mido: Build ipacm from source
    Signed-off-by: Jyotiraditya <dreadnaught02@outlook.com>
    Signed-off-by: RahifM <faizel326@gmail.com>

--- device.mk
+++ device.mk
@@ -214,10 +214,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
 
-# IPA Manager
-PRODUCT_PACKAGES += \
-    ipacm \
-    IPACM_cfg.xml
+# IPACM Data
+include $(TOPDIR)vendor/qcom/opensource/data-ipa-cfg-mgr/ipacm_vendor_product.mk
 
 # IRQ
 PRODUCT_COPY_FILES += \
