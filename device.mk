ce59487369d634edbfcdb8aaf0bf8c683e90c378
TheStrix <parthbhatia98@gmail.com>
2020-04-09 07:44:29 +0700

    nido: Build vendor/common modules
    - From vendor/qcom/common repos

--- device.mk
+++ device.mk
@@ -345,6 +345,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
 
+# Vendor-common modules
+TARGET_COMMON_QTI_COMPONENTS := \
+    perf
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
