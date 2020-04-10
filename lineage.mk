9c0426f3509c06347021eca3ba9f94f37ab17ffb
Andrew <poisk567@gmail.com>
2020-04-10 16:09:41 +0300

    init

---
+++ lineage.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/meizu/m1721/full_m1721.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_m1721
+BOARD_VENDOR := Meizu
+
+PRODUCT_GMS_CLIENTID_BASE := android-meizu
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="meizu/m1721/m1721:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys" \
+    PRIVATE_BUILD_DESC="m1721-user 7.0 NRD90M V8.5.4.0.NCFMIED release-keys"
