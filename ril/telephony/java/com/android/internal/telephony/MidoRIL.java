c239ac6275685eba29ea6821c769fd2d79dc4148
Teledurak <romashkarus08@gmail.com>
2020-06-24 14:02:42 +0300

    m1721: Some rebrand
    Change-Id: I1033e19c51f07ec1dfbfcf5178744d53d5380779

--- ril/telephony/java/com/android/internal/telephony/MidoRIL.java
+++ ril/telephony/java/com/android/internal/telephony/MidoRIL.java
@@ -23,17 +23,17 @@ import android.os.Parcel;
 import static com.android.internal.telephony.RILConstants.*;
 
 /**
- * Custom Qualcomm RIL for Xiaomi Redmi Note 4
+ * Custom Qualcomm RIL for Meizu M6 Note
  *
  * {@hide}
  */
-public class MidoRIL extends RIL implements CommandsInterface {
+public class MeizuRIL extends RIL implements CommandsInterface {
 
-    public MidoRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
+    public MeizuRIL(Context context, int preferredNetworkType, int cdmaSubscription) {
         this(context, preferredNetworkType, cdmaSubscription, null);
     }
 
-    public MidoRIL(Context context, int preferredNetworkType,
+    public MeizuRIL(Context context, int preferredNetworkType,
             int cdmaSubscription, Integer instanceId) {
         super(context, preferredNetworkType, cdmaSubscription, instanceId);
     }
