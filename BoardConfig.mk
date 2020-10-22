74e3b35b73ac045c0bdba9e8d45a409c332e081e
fakeyatogod <fakeyatokami@gmail.com>
2020-10-22 15:03:20 +0000

    m1721: Set TARGET_HAS_BROKEN_WLAN_SET_INTERFACE to true
    To fix tethering/hotspot with lastest caf wlan
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,6 +199,7 @@ WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_HIDL_UNIFIED_SUPPLICANT_SERVICE_RC_ENTRY := true
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+TARGET_HAS_BROKEN_WLAN_SET_INTERFACE := true
 
 # Inherit from the proprietary version
 -include vendor/meizu/m1721/BoardConfigVendor.mk
