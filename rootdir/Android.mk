51d09a03c9b89b55fe7f44a00c7171264f47b4d8
Roman Rihter <romashkarus08@gmail.com>
2021-02-06 18:08:06 +0300

    m1721: squash custom inits to init.qcom.rc
    * it seems custom inits were not copying in userdebug build
    
    Change-Id: Icb6229ac431081eca0fde3bdf1f5ea58a089894c

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -19,22 +19,6 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.fingerprint.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.fingerprint.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.camera.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.camera.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
@@ -49,7 +33,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.target.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
