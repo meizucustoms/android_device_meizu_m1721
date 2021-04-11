c1e6e42ec8d33edab508ba8fa13cc17361a4f1d5
Roman Rihter <romashkarus08@gmail.com>
2021-04-11 20:55:08 +0300

    m1721: squash
    * I am too lazy to sort that shet :<

--- qcom-caf/audio/Android.mk
+++
@@ -1,9 +0,0 @@
-MY_LOCAL_PATH := $(call my-dir)
-
-include $(MY_LOCAL_PATH)/hal/Android.mk
-include $(MY_LOCAL_PATH)/voice_processing/Android.mk
-include $(MY_LOCAL_PATH)/mm-audio/Android.mk
-include $(MY_LOCAL_PATH)/visualizer/Android.mk
-include $(MY_LOCAL_PATH)/post_proc/Android.mk
-include $(MY_LOCAL_PATH)/qahw/Android.mk
-include $(MY_LOCAL_PATH)/qahw_api/Android.mk
