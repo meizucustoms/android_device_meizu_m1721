7b31942b3cc2fbb80895243b7e05365fa085e57c
Roman Rihter <romashkarus08@gmail.com>
2021-05-10 15:44:42 +0300

    m1721: vendor/cm -> vendor/lineage
    This path was changed in Oreo
    
    Change-Id: I9ed7a0718f08c755e64cce9e68863351971fe885

--- lineage.mk
+++ lineage.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/meizu/m1721/full_m1721.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_m1721
 BOARD_VENDOR := Meizu
