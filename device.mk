ab380ffb01d3eec135308a09c844011535d86d43
Erfan Abdi <erfangplus@gmail.com>
2021-09-10 20:54:14 +0200

    m1721: move to in-tree HALs
    Change-Id: I5aa47f2e12477d6cc74cda2d990470e8a673fcaf

--- device.mk
+++ device.mk
@@ -45,6 +45,9 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
+    
+QCOM_SOONG_NAMESPACE := \
+    $(LOCAL_PATH)/qcom-caf
 
 # Permissions
 PRODUCT_COPY_FILES += \
