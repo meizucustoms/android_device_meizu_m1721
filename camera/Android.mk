1d00fb8122d36cacfe5a6c1c8a7de6a595f4adab
Roman Rihter <teledurak@gmail.com>
2021-10-07 13:10:07 +0300

    m1721: Add camera wrapper and Flyme camera libraries
    OSS camera HAL gave very many problems to us, so just nuke it
    until good times. :(

--- camera/Android.mk
+++ camera/Android.mk
@@ -1,27 +1,20 @@
-MM_V4L2_DRIVER_LIST += msm8960
-MM_V4L2_DRIVER_LIST += msm8974
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8226
-MM_V4L2_DRIVER_LIST += msm8610
-MM_V4L2_DRIVER_LIST += apq8084
-MM_V4L2_DRIVER_LIST += mpq8092
-MM_V4L2_DRIVER_LIST += msm_bronze
-MM_V4L2_DRIVER_LIST += msm8916
-MM_V4L2_DRIVER_LIST += msm8994
-MM_V4L2_DRIVER_LIST += msm8084
-MM_V4L2_DRIVER_LIST += msm8909
-MM_V4L2_DRIVER_LIST += msm8952
-MM_V4L2_DRIVER_LIST += msm8996
-MM_V4L2_DRIVER_LIST += msm8992
-MM_V4L2_DRIVER_LIST += msm8937
-MM_V4L2_DRIVER_LIST += msm8953
-MM_V4L2_DRIVER_LIST += msmcobalt
-MM_V4L2_DRIVER_LIST += msmfalcon
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
 
-ifneq (,$(filter $(MM_V4L2_DRIVER_LIST),$(TARGET_BOARD_PLATFORM)))
-  ifneq ($(strip $(USE_CAMERA_STUB)),true)
-    ifneq ($(BUILD_TINY_ANDROID),true)
-      include $(call all-subdir-makefiles)
-    endif
-  endif
-endif
+LOCAL_C_INCLUDES := system/media/camera/include
+
+LOCAL_SRC_FILES := \
+    CameraWrapper.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libcamera_client libutils libcutils libhidlbase libhidltransport
+
+LOCAL_STATIC_LIBRARIES := \
+    libbase \
+    libarect
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY) 
--- device.mk
+++ device.mk
@@ -135,19 +135,13 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
-    android.hardware.camera.common@1.0 \
-    android.hardware.camera.device@3.3:64 \
-    android.hardware.camera.device@3.4:64 \
-    android.hardware.camera.device@3.5:64 \
-    android.hardware.camera.provider@2.4 \
+    camera.device@1.0-impl \
+    camera.device@3.2-impl:32 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5:64 \
-    android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
-    camera.msm8953 \
+    vendor.qti.hardware.camera.device@1.0_vendor \
     libcamshim \
-    libmm-qcamera \
     libui_shim \
     Snap
 
