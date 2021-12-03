88a74e58311797f00a4e3015f604cc7e2f601afe
Roman Rihter <teledurak@gmail.com>
2021-12-03 19:59:27 +0300

    m1721: parts: remove sound settings
    Change-Id: If9114573625bcccad8aedbce2ec0e07a2737bfc6

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,10 +22,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
 import android.util.Log;
-import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.preferences.FileUtils;
-import org.lineageos.settings.soundcontrol.SoundControlSettings;
 import org.lineageos.settings.torch.TorchSettings;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -40,7 +38,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         Log.d(TAG, "Starting Doze service");
       DozeUtils.startService(context);
     }
-    new DiracUtils(context).onBootCompleted();
 
     FileUtils.setValue(
         TorchSettings.TORCH_1_BRIGHTNESS_PATH,
@@ -50,18 +47,5 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         TorchSettings.TORCH_2_BRIGHTNESS_PATH,
         Settings.Secure.getInt(context.getContentResolver(),
                                TorchSettings.KEY_YELLOW_TORCH_BRIGHTNESS, 100));
-    int gain =
-        Settings.Secure.getInt(context.getContentResolver(),
-                               SoundControlSettings.PREF_HEADPHONE_GAIN, 4);
-    FileUtils.setValue(SoundControlSettings.HEADPHONE_GAIN_PATH,
-                       gain + " " + gain);
-    FileUtils.setValue(
-        SoundControlSettings.MICROPHONE_GAIN_PATH,
-        Settings.Secure.getInt(context.getContentResolver(),
-                               SoundControlSettings.PREF_MICROPHONE_GAIN, 0));
-    FileUtils.setValue(
-        SoundControlSettings.SPEAKER_GAIN_PATH,
-        Settings.Secure.getInt(context.getContentResolver(),
-                               SoundControlSettings.PREF_SPEAKER_GAIN, 0));
   }
 }
