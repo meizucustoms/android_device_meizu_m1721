52bd48aed4ce0f3493d540a9fa1c261c9d58c018
shivatejapeddi <shivatejapeddi@gmail.com>
2021-12-13 10:40:27 +0700

    m1721: Copy Modified libprocessgroup configs to vendor
    * Schedtune paths are depreceated from Android S and updated to utilclamp
    as our kernel doesnt support utilclamp, these configs are modified to fix lags and stutters
    since power hal and perf stack are still dependent on schedtune
    
    Signed-off-by: Andrey Sprynga <rtx4d@aospa.co>

--- device.mk
+++ device.mk
@@ -261,6 +261,11 @@ PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl:64 \
     android.hardware.keymaster@3.0-service
 
+# libprocessgroup
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
+
 # Lights
 PRODUCT_PACKAGES += \
     android.hardware.light@2.0-service.meizu_m1721
