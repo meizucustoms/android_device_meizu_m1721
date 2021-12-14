cedee1c24bbca44157737b5a72170ace35bf7012
ZeeLog <Danjel.Zammit@gmail.com>
2021-12-14 23:34:54 +0100

    m1721: Remove whitelisted app left over
    * this is now being copied from vendor

--- device.mk
+++ device.mk
@@ -419,10 +419,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libstdc++.vendor
 
-# Whitelisted apps
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml
-
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy \
