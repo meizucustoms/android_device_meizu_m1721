952ad22625c2adac2b78b9516421eb26258ebb4e
Roman Rihter <teledurak@gmail.com>
2022-02-28 19:57:08 +0300

    m1721: AOSPA-ify overlays
    Change-Id: I4a5e7fb709bc0e3fd5d00737d1a94c0281b96465

--- device.mk
+++ device.mk
@@ -19,8 +19,9 @@ $(call inherit-product, vendor/meizu/m1721/m1721-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aospa
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
