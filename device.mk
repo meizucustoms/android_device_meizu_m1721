254d0d564a631757eed88753edee8690caeee9a4
Roman Rihter <teledurak@gmail.com>
2022-02-28 18:12:51 +0300

    m1721: use QTI common bt component
    Change-Id: I2cb50b7ae74a6f0843959a02ab6e46320c2ff08d

--- device.mk
+++ device.mk
@@ -131,9 +131,24 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml
 
 # Bluetooth
+TARGET_USE_QTI_BT_STACK := false
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.btconfigstore@1.0.vendor:64 \
-    vendor.qti.hardware.btconfigstore@2.0.vendor:64
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth.audio@2.0-impl \
+    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio@2.1.vendor \
+    android.hardware.bluetooth.a2dp@1.0.vendor \
+    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
+    audio.bluetooth_qti.default:32 \
+    audio.bluetooth.default:32 \
+    liba2dpoffload \
+    libbluetooth_qti \
+    libbthost_if \
+    libbtconfigstore
 
 # Camera
 PRODUCT_PACKAGES += \
@@ -322,6 +337,7 @@ PRODUCT_PACKAGES += \
 TARGET_COMMON_QTI_COMPONENTS += \
     adreno-legacy \
     av \
+    bt \
     overlay \
     perf \
     telephony \
