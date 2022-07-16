c4abaa370d0ce93123bcc16b6e784b9e4c2373be
Roman Rihter <teledurak@gmail.com>
2022-07-17 02:17:28 +0300

    m1721: vendorsetup: proper zsh support
    Also remove "shell killer" (exit $?)
    
    Change-Id: I9ab88eac92765c0972ac440d52cd172ae3345cb2

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+if [ "$(grep zsh <<< "$SHELL")" ]; then
+DIR="$(cd -P -- "$(dirname -- "${(%):-%N}")" && pwd -P)"
+else
 DIR="$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)"
+fi
 
 echo 'Downloading latest prebuilt Camera.apk from GrapheneOS...'
 camera_dir=/tmp/GrapheneOSCamera
 camera_branch=12.1
 rm -rf $camera_dir &&
 git clone --depth=1 https://github.com/GrapheneOS/platform_external_Camera.git -b $camera_branch $camera_dir &&
-mv $camera_dir/prebuilt/Camera.apk "$DIR"/GrapheneCamera/ || exit $?
+mv $camera_dir/prebuilt/Camera.apk "$DIR"/GrapheneCamera/ || echo "Download failed with status code $?"
